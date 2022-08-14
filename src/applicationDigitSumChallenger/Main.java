c8a4bd57ced6c0d836c4e2cf7bc1e3da0411ca2d
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-08-14 12:55:39 -0300

    cria a logica dentro do get para somar os algarismos

--- src/applicationDigitSumChallenger/Main.java
+++ src/applicationDigitSumChallenger/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args) {
         // quando tive um output dentro do get ouset ou construtor, não é necessario inserir outro output aqui
         DigitSumChallenger digit = new DigitSumChallenger();
+        digit.setDigit(-1);
+        System.out.println(digit.getDigit());
+        digit.setDigit(9);
+        System.out.println(digit.getDigit());
+        digit.setDigit(11);
+        System.out.println(digit.getDigit());
+        digit.setDigit(285);
+        System.out.println(digit.getDigit());
 
     }
 }
--- src/entitiesDigitSumChallenger/DigitSumChallenger.java
+++ src/entitiesDigitSumChallenger/DigitSumChallenger.java
@@ -8,10 +8,45 @@ public class DigitSumChallenger {
         System.out.println("Empty constructor called");
 
     }
+    // overloading constructors
     public DigitSumChallenger(int digit) {
         this.digit = digit;
     }
-     /*Write a method with the name sumDigits that has one int parameter called number.If parameter is >= 10 then the
+
+/*    public int sumDigit(int digit) {
+        if (this.digit < 10) {
+
+            return -1;
+        }
+        int sum = 0;
+        while (this.digit > 0) {
+            int singleDigit = this.digit % 10;
+            sum += singleDigit;
+            this.digit /= 10;
+        }
+        digit = sum;
+        return digit;
+    }*/
+    public int getDigit() {
+        if (digit < 10) {
+
+            return -1;
+        }
+        int sum = 0;
+        while (digit > 0) {
+            int singleDigit = digit % 10;
+            sum += singleDigit;
+            digit /= 10;
+
+        }
+        return sum;
+    }
+
+    public void setDigit(int digit) {
+
+        this.digit = digit;
+    }
+    /*Write a method with the name sumDigits that has one int parameter called number.If parameter is >= 10 then the
     method should process the number and return sum of all digits, otherwise return -1 to indicate an invalid value.
     The numbers from 0-9 have 1 digit so we don't want to process them, also we don't want to process negative numbers,
     so also return -1 for negative numbers.
